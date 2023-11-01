edc288c0f7b9d7522448e553fb3282aee77bc9ff
MavithaShehar <mavithashehar@gmail.com>
2023-11-01 21:02:10 +0530

    bug fix vax section

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,28 +22,40 @@ $(window).ready(() =>{
 //*************** mobile nav bar ***********************
 
 $("#navbar-mobile-icon").on("click" , () => {
-    $("header > nav").css("display" , "flex");
-    $("header > nav > a").css("display" , "block");
-    $("#navbar-mobile-icon").css("display" , "none");
-    $("#navbar-close-icon").css("display" , "block");
-    $("#navbar-bg").css("display" , "block");
+    $("header > nav").addClass("show_flex");
+    $("#navbar-mobile-icon").addClass("hide");
+    $("#navbar-close-icon").addClass("show");
+    $("#navbar-bg").addClass("show");
 
 });
 
 $("#navbar-close-icon").on("click" , () => {
-    $("header > nav").css("display" , "none");
-    $("#navbar-close-icon").css("display" , "none");
-    $("#navbar-mobile-icon").css("display" , "block");
-    $("#navbar-bg").css("display" , "none");
+    $("header > nav").removeClass("show_flex")
+    $("#navbar-mobile-icon").removeClass("hide");
+    $("#navbar-close-icon").removeClass("show");
+    $("#navbar-bg").removeClass("show");
 
 });
 
+$("nav > a").on("click" , () => {
+    $("#navbar-close-icon").click();
 
-$(".mobile").on("click",()=> {
-    $("#navbar-bg").css("display", "none");
-    $("header > nav").css("display", "none");
-    $("#navbar-mobile-icon").css("display" , "block");
-    $("#navbar-close-icon").css("display" , "none");
 });
+
+// $(".mobile").on("click", () => {
+//     if (window.matchMedia("(max-width: 500px)").matches) {
+//         $("#navbar-bg").css("display", "none");
+//         $("header > nav").css("display", "none");
+//         $("#navbar-mobile-icon").css("display", "block");
+//         $("#navbar-close-icon").css("display", "none");
+//     }
+// });
+
+// $(".mobile").on("click", () => {
+//     if (window.matchMedia("(max-width: 600px)").matches) {
+//         $("#navbar-mobile-icon").css("display", "none");
+//         $("#navbar-close-icon").css("display", "none");
+//     }
+// });
 
 
